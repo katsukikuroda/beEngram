9219703ffcf8ce628b4b8a1fa5fccbe3860c8e75
k_kuroda <katsukikuroda@gmail.com>
2022-01-22 20:52:40 +0900

    initial commit

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, User
+from .models import Comment, Post, User
 
 admin.site.register(User)
 admin.site.register(Post)
+admin.site.register(Comment)
+
